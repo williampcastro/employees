4bd65df786e0fcb26275501e19c3291c5198204c
will <willianpcastro@hotmail.com>
2020-09-17 11:37:14 -0300

    :construction: get employees from array

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,79 @@
 <template>
   <div id="app">
 
-    <h1>Employess</h1>
+    <h1>Employees</h1>
 
-    <table>
+    <table v-if="employees.length">
       <thead>
-        <tr>
-          <th>Name</th>
-          <th>E-mail</th>
-          <th>Sector</th>
-          <th>Actions</th>
-        </tr>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Sector</th>
+        <th>Actions</th>
+      </tr>
       </thead>
       <tbody>
 
-        <tr>
+      <tr v-for="(employee, index) in employees">
 
-          <td>William</td>
-          <td>wila@email.com</td>
-          <td>Product</td>
+        <td> {{ index + 1 }}</td>
+        <td>{{ employee.name ? employee.name : '--' }}</td>
+        <td>{{ employee.phoneNumber ? employee.phoneNumber : '--' }}</td>
+        <td>{{ employee.email ? employee.email : '--' }}</td>
+        <td>{{ employee.sector ? employee.sector : '--' }}</td>
 
-          <td>
+        <td>
 
-            <button>Editar</button>
-            <button>Remover</button>
+          <button :id="'button_update_' + index" v-show="!buttonSaveIsAble" @click="showButtonSave()">Update</button>
+          <button :id="'button_save_' + index" v-show="buttonSaveIsAble" @click="updateEmployee()">Save</button>
+          <button :id="'button_remove_' + index" @click="removeEmployee(index)">Remove</button>
 
-          </td>
-        </tr>
+        </td>
+      </tr>
       </tbody>
     </table>
 
+    <div v-else> Not employees registered!</div>
 
   </div>
 </template>
 
 <script>
+
+const invertBooleanValue = function (data) {
+  return !data;
+}
+
+const employees = [];
+employees.push( { name: 'Will_1', phoneNumber: '9 9999-1111', email: 'will@will.com', sector: 'DEV' })
+employees.push( { name: 'Will_2', phoneNumber: '9 9999-1111', email: 'will@will.com', sector: null })
+employees.push( { name: 'Will_3', phoneNumber: null, email: 'will@will.com', sector: 'DEV' })
+
+
 export default {
   name: 'app',
-  data () {
 
+  methods: {
+    removeEmployee: function (index) {
+      this.$delete (this.employees, index)
+    },
+    updateEmployee: function () {
+      this.buttonSaveIsAble = true;
+      this.buttonSaveIsAble = invertBooleanValue(this.buttonSaveIsAble)
+    },
+    showButtonSave: function () {
+      this.buttonSaveIsAble = invertBooleanValue(this.buttonSaveIsAble)
+    },
+  },
+
+  data () {
+    return {
+      buttonSaveIsAble: false,
+      employees,
+    }
   }
+
 }
 </script>
 
